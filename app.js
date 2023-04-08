df1e30f15cc738de6e84b6392866fc68131a3748
cesinha1 <cesardepaula80@gmail.com>
2023-04-08 11:40:35 -0300

    montando as rotas, organizando projeto

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 // carregando módulos
 const express = require('express');
-const handlebars = require('express-handlebars');
+const { engine } = require('express-handlebars');
 const bodyParser = require('body-parser');
 const app = express();
+const admin = require('./routes/admin');
 // const mongoose = require('mongoose');
 // Configuração
     // Body Parser
         app.use(bodyParser.urlencoded({extended: true}));
         app.use(bodyParser.json());
     // Handlebars
-        app.engine('handlebars', handlebars({defaultLayout: 'main'}));
-        app.set('view engine', 'handlebars');
+    app.engine('handlebars', engine({defaultLayout: 'main'}));
+    app.set('view engine', 'handlebars');
     // Mongoose
     
 // Rotas
-
+    app.use('/admin', admin);
 // Outros
 const PORT = 6032;
 app.listen(PORT, () => {
