a7c697cb6e5d2152e05c163ca7740b71e5eff20c
cesinha1 <cesardepaula80@gmail.com>
2023-04-10 14:22:14 -0300

    inserindo categorias no banco de dados, mongoDB

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+require("../models/Categoria");
+const Categoria = mongoose.model("categorias");
+
 
 router.get('/', (req, res) => {
     res.render("admin/index");
@@ -15,6 +19,19 @@ router.get('/categorias', (req, res) => {
 
 router.get('/categorias/add', (req, res) => {
     res.render("admin/addcategorias");
+});
+
+router.post('/categorias/nova', (req, res) => {
+    const novaCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug
+    }
+
+    new Categoria(novaCategoria).save().then(() => {
+        console.log("categoria salva com sucesso!");
+    }).catch((err) => {
+        console.log("Erro ao salvar categoria: " + err);
+    });
 });
 
 module.exports = router;
