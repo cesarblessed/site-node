06087c5b62238a9908f177bce3ddaa9264651f8c
cesinha1 <cesardepaula80@gmail.com>
2023-04-14 09:49:20 -0300

    listando categorias da aplicação

--- routes/admin.js
+++ routes/admin.js
@@ -14,7 +14,12 @@ router.get('/posts', (req, res) => {
 });
 
 router.get('/categorias', (req, res) => {
-    res.render("admin/categorias");
+    Categoria.find().sort({date: 'desc'}).lean().then((categorias) => {
+        res.render('admin/categorias', {categorias: categorias})
+    }).catch((err) => {
+        req.flash('error_msg', 'Erro ao listar categorias')
+        res.redirect('/admin')
+    });
 });
 
 router.get('/categorias/add', (req, res) => {
