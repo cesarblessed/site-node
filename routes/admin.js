53a14502709b629cb26bd2102a1469f755bd534a
cesinha1 <cesardepaula80@gmail.com>
2023-04-14 19:04:14 -0300

    editando categorias do projeto

--- routes/admin.js
+++ routes/admin.js
@@ -62,4 +62,34 @@ router.post('/categorias/nova', (req, res) => {
     
 });
 
+router.get("/categorias/edit/:id", (req, res) => {
+    Categoria.findOne({_id: req.params.id}).lean().then((categoria) => {
+        res.render("admin/editcategorias", {categoria: categoria});
+    }).catch((err) => {
+        req.flash("error_msg", "Esta categoria não existe");
+        res.redirect("/admin/categorias");
+    });
+    
+});
+
+router.post("/categorias/edit", (req, res) => {
+    Categoria.findOne({_id: req.body.id}).then((categoria) => {
+
+        categoria.nome = req.body.nome
+        categoria.slug = req.body.slug
+
+        categoria.save().then(() => {
+            req.flash('success_msg', 'Categoria editada com Sucesso!')
+            res.redirect('/admin/categorias')
+        }).catch( err => { // ERR: Erro ao salvar::editar
+            req.flash('error_msg', 'Houve um erro interno ao editar categoria.')
+            res.redirect("/admin/categorias")
+        })
+
+    }).catch( err => { // ERR: Não encontrou a categoria
+        req.flash('error_msg', 'Houve um erro ao editar categoria.') //Mensagem de Erro
+        res.redirect("/admin/categorias")
+    })
+});
+
 module.exports = router;
