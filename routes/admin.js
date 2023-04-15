dbdb744d7b90292c3cacb2d184754dec663c96bb
cesinha1 <cesardepaula80@gmail.com>
2023-04-15 09:37:53 -0300

    formulario de postagens

--- routes/admin.js
+++ routes/admin.js
@@ -102,4 +102,18 @@ router.post("/categorias/deletar", (req, res) => {
     });
 });
 
+router.get("/postagens", (req, res) => {
+    res.render("admin/postagens");
+});
+
+router.get("/postagens/add", (req, res) => {
+    Categoria.find().lean().then((categorias) => {
+        res.render("admin/addpostagem", {categorias: categorias});
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao carregar o formulário");
+        res.redirect("/admin");
+    });
+   
+});
+
 module.exports = router;
