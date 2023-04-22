d4d6a378b86ce7d72cd6d76d147f46b9b262cd2a
cesinha1 <cesardepaula80@gmail.com>
2023-04-22 09:27:32 -0300

    home page de postagens

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 const session = require("express-session");
 const flash = require("connect-flash");
+require("./models/Postagem");
+const Postagem = mongoose.model("postagens");
 
 // Configuração
     // Sessão
@@ -51,9 +53,17 @@ const flash = require("connect-flash");
         // Rotas
 
     app.get('/', (req, res) => {
-        res.send('Rota principal');
-    })
+        Postagem.find().populate("categoria").lean().sort({data: "desc"}).then((postagens) => {
+            res.render("index", {postagens: postagens});
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno!");
+            res.redirect("/404");
+        });
+    });
 
+    app.get("/404", (req, res) => {
+        res.send("erro 404!");
+    });
     
     app.get('/posts', (req, res) => {
         res.send('Lista Posts');
