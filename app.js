68be5fd3e33e1c9c01dbd5f1ebdcc06da92739f3
cesinha1 <cesardepaula80@gmail.com>
2023-04-26 09:31:32 -0300

    listagem de categorias

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const session = require("express-session");
 const flash = require("connect-flash");
 require("./models/Postagem");
 const Postagem = mongoose.model("postagens");
+require("./models/Categoria");
+const Categoria = mongoose.model("categorias");
 
 // Configuração
     // Sessão
@@ -75,13 +77,39 @@ const Postagem = mongoose.model("postagens");
         });
     });
 
+    app.get("/categorias", (req, res) => {
+        Categoria.find().lean().then((categorias) => {
+            res.render("categorias/index", {categorias: categorias});
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro ao listar as categorias");
+            res.redirect("/");
+        });
+    });
+
+    app.get("/categorias/:slug", (req, res) => {
+        Categoria.findOne({slug: req.params.slug}).lean().then((categoria) => {
+            if(categoria){
+                Postagem.find({categoria: categoria._id}).lean().then((postagens) => {
+                    res.render("categorias/postagens", {postagens: postagens, categoria: categoria});
+                }).catch((err) => {
+                    req.flash("error_msg","Houve um erro ao listar os posts!");
+                    res.redirect("/");
+                });
+            }else{
+                req.flash("error_msg", "Esta categoria não existe");
+                res.redirect("/");
+            }
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno ao carregar a página desta categoria");
+            res.redirect("/");
+        });
+    });
+
     app.get("/404", (req, res) => {
         res.send("erro 404!");
     });
     
-    app.get('/posts', (req, res) => {
-        res.send('Lista Posts');
-    })
+    
 
     app.use('/admin', admin);
 // Outros
